2953077dcd413b0e3cd22632a6707249297ea1a8
ruinalen <ruinalen@gmail.com>
2017-06-08 17:11:41 +0900

    iphone url hide update

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, minimum-scale=1">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>ssulchat</title>
     {{--<link rel="stylesheet" type="text/css" href="/css/app.css">--}}

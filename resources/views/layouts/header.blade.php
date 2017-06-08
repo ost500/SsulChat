045f0809e26427485da1c1c28b6e6be52f4e7da3
ruinalen <ruinalen@gmail.com>
2017-06-08 12:25:31 +0900

    SCROLL design update

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -12,7 +12,6 @@
     <link rel="stylesheet" type="text/css" href="/css/common.css">
     <link rel="stylesheet" type="text/css" href="/css/jquery.jscrollpane.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script type="text/javascript" src="/js/jquery.jscrollpane.min.js"></script>
 </head>
 
 @yield('content')

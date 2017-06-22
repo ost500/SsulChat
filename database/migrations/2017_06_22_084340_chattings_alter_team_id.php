e5219a7b45523a8289dd3de829876ae218fbff66
OST_MAC <ost5253@gmail.com>
2017-06-22 09:45:02 +0900

    team_id nullable() 마이그레이션

---
+++ database/migrations/2017_06_22_084340_chattings_alter_team_id.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChattingsAlterTeamId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('chattings', function (Blueprint $table) {
+            $table->dropForeign(['team_id']);
+            $table->dropColumn('team_id');
+
+        });
+        Schema::table('chattings', function (Blueprint $table) {
+            $table->integer('team_id')->unsigned()->index()->nullable()->after('user_id');
+            $table->foreign('team_id')->references('id')->on('teams');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('chattings', function (Blueprint $table) {
+            $table->dropForeign('chattings_team_id_foreign');
+            $table->dropColumn('team_id');
+
+        });
+        Schema::table('chattings', function (Blueprint $table) {
+            $table->integer('team_id')->unsigned()->index();
+            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
+        });
+    }
+}

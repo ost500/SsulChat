5c2f32a88c670932679f858b9417f4350744249a
ruinalen <ruinalen@gmail.com>
2017-06-25 17:12:09 +0900

    chatting blade 크기 조정

--- resources/views/chatting.blade.php
+++ resources/views/chatting.blade.php
@@ -12,6 +12,7 @@
     <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <script>
         window.onload = function () {
+            $(window).scrollTo(0,1);
             if (matchMedia("only screen and (max-device-width: 480px) and (min-device-width: 320px)").matches) {
                 var outerheight = $(window).outerHeight(true) - $('.header_chat').outerHeight(true) - $('.chat_input_wrap').outerHeight(true) - 20
                 var inner = $('.chat_txt_area2');
@@ -42,6 +43,7 @@
 
     <script>
         $(window).resize(function () {
+            $(window).scrollTo(0,1);
             if (matchMedia("only screen and (max-device-width: 480px) and (min-device-width: 320px)").matches) {
                 var outerheight = $(window).outerHeight(true) - $('.chat_txt_area1').outerHeight(true) - $('.chat_input_wrap').outerHeight(true) - 20;
                 var inner = $('.chat_txt_area2');

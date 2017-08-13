4277b341f4540d551a047e29ffaa154e54b5de28
OST_MAC <ost5253@gmail.com>
2017-08-13 15:52:00 +0900

    애드센스

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,14 @@
     <link rel="shortcut icon" href="/images/favicon.ico">
 
     <link rel="stylesheet" href="/css/business.style.css">
+
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-8665007420370986",
+            enable_page_level_ads: true
+        });
+    </script>
 </head>
 
 <body class="gl-business-template gl-home-template @if(isset($loginView)) gl-show-menu @endif">

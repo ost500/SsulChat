cafe3fb92e57eeeffaad4ac7072bd22007c09105
OST_MAC <ost5253@gmail.com>
2017-08-17 02:25:38 +0900

    page 레이아웃 변경

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -42,7 +42,7 @@
 
 
     <!-- PAGE CONTETNT -->
-    <section class="gl-page-content-section">
+    <section class="gl-page-content-section gl-section-wrapper">
 
         <div class="container">
 
@@ -54,107 +54,206 @@
                 <div class="col-sm-6">
                     <div id="chartdiv2"></div>
                 </div>
-                <div class="gl-element-type-wrapper">
-
-                    <h3>연관 검색어</h3>
-                    <div class="gl-element-wrapper gl-elements-page" style="margin-bottom: 130px;">
-                        @foreach($morphs as $morph)
-                            <a href="#" class="gl-tag-btn gl-design">{{ $morph->morph }} ({{ $morph->morph_count }})</a>
-                        @endforeach
-                    </div>
-                </div>
-
-                <!-- SIDEBAR -->
-                <div class="gl-sidebar gl-page-sidebar col-md-4 col-sm-4 col-xs-12">
-                    <div class="gl-sidebar-widget gl-category-widget">
-                        <h3 class="gl-sidebar-title">채팅방</h3>
-
-                        <ul>
-                            @foreach($page->ssuls as $ssul)
-                                <li><a href="{{ route('chattings', ['id' => $ssul->id]) }}">{{ $ssul->name }}</a></li>
-                            @endforeach
-                        </ul>
-                    </div>
-
-
-                </div>
-                <!-- SIDEBAR END -->
-                <!-- PAGE CONTENT DETAILS -->
-                <div class="gl-page-content tab-content col-md-8 col-sm-8 col-xs-12">
-
-                    <!-- GALLERY TAB -->
-                    <div role="tabpanel" class="tab-pane fade in active" id="overview">
-                        <!-- GALLERY -->
-                        <div class="gl-profile-gallery">
-                            <ul class="gl-gallery">
-
-
-                            @foreach($page->pagePosts as $post)
-                                <!-- FEATURED ITEMS -->
-                                    <div class="gl-featured-items col-md-6 col-sm-6 col-xs-12">
-                                        <div class="gl-feat-items-img-wrapper">
-                                            <img src="{{ $post->main_photo }}" alt="Featured Listing"
-                                                 class="gl-lazy">
-                                        </div>
-
-                                        <div class="gl-feat-item-details">
-                                            {{--<span class="gl-item-rating">--}}
-                                            {{--<i class="ion-android-star-outline"></i>--}}
-                                            {{--4.5--}}
-                                            {{--</span>--}}
-                                            <h3>
-                                                <a href="{{ route('pages.posts',['id'=>$post->id]) }}">{{ $post->message }}</a>
-                                            </h3>
-
-                                        </div>
-                                    </div>
-                                    <!-- END -->
-                                @endforeach
-
-                            </ul>
-                        </div>
-                        <!-- END -->
-
-
-                        <!-- REVIEWS / COMMENT DETAILS -->
-                        <div class="gl-review-details appear fadeIn" data-wow-duration="1s" data-wow-delay=".3s">
-                            <h3 class="gl-content-title">이번 주 베스트 챗</h3>
-                        @foreach($likeBests as $likeBest)
-                            <!-- Reviews -->
-                                <div class="gl-reviews">
-                                    <!-- USER IMG -->
-                                    <div class="gl-user-img">
-                                        <img src="{{ $likeBest->profile_img }}" alt="User" class="gl-lazy">
-                                    </div>
-                                    <!-- END -->
-
-                                    <!-- TEXT -->
-                                    <div class="gl-review-text">
-                                        {{--<img src="{{ $likeBest->picture }}">--}}
-                                        <h3>{{ $likeBest->name }}</h3>
-                                        <img src="{{ $likeBest->picture }}">
-                                        <p>{{ $likeBest->content }}</p>
-                                        <span class="gl-item-rating"><i
-                                                    class="ion-ios-star"></i>{{ $likeBest->likeCount }}</span>
-                                    </div>
-                                    <!-- END -->
-                                </div>
-                                <!-- END -->
-                            @endforeach
-                        </div>
-                        <!-- END -->
-
-                    </div>
-                    <!-- GALLERY TAB END -->
-
-                </div>
-                <!-- PAGE CONTENT DETAILS END -->
 
 
             </div>
         </div>
     </section>
     <!-- PAGE CONTETNT END -->
+
+
+    <section class="gl-feat-listing-section gl-section-wrapper">
+        <div class="container">
+
+            <!-- SECTION HEADINGS -->
+            <div class="gl-section-headings">
+                <h1>{{$page->title}} 연관 검색어</h1>
+                <p>오늘의 핫이슈</p>
+            </div>
+            <!-- END -->
+
+            <!-- WRAPPER -->
+            <div class="gl-featured-listing-wrapper">
+                <div class="gl-element-wrapper gl-elements-page" style="margin-bottom: 130px;">
+                    @foreach($morphs as $morph)
+                        <a href="#" class="gl-tag-btn gl-design">{{ $morph->morph }} ({{ $morph->morph_count }})</a>
+                    @endforeach
+                </div>
+
+            </div>
+
+
+        </div>
+
+    </section>
+    <section class="gl-feat-listing-section gl-section-wrapper">
+        <div class="container">
+
+            <!-- SECTION HEADINGS -->
+            <div class="gl-section-headings">
+                <h1>{{$page->title}} 포스트</h1>
+                <p>오늘의 핫이슈</p>
+            </div>
+            <!-- END -->
+
+            <!-- WRAPPER -->
+            <div class="gl-featured-listing-wrapper">
+                @foreach($page->pagePosts as $num => $postItem)
+                    @if(($num + 1) % 4 == 0)
+                        <div class="row">
+
+                            @endif
+                            <div onclick="location.href='{{ route('pages', ['id' => $postItem->id]) }}'"
+                                 class="gl-featured-items gl-featured-items-alt col-md-3 col-sm-3 col-xs-6">
+                                <div class="gl-feat-items-img-wrapper">
+                                    <picture>
+                                        <source media="(min-width: 768px)" src="{{ $postItem->main_photo }}">
+                                        <img alt="{{ $page->title }}" src="{{ $postItem->main_photo }}">
+                                    </picture>
+
+
+                                </div>
+
+                                <div class="gl-feat-item-details">
+                                    <h3>
+                                        <a href="{{ route('pages', ['id' => $postItem->id]) }}">{{ str_limit($postItem->message, 20) }}</a>
+                                    </h3>
+                                    {{--<div class="gl-item-location">--}}
+
+                                    {{--<span>{{ $page->description }}</span>--}}
+                                    {{--</div>--}}
+
+
+                                </div>
+
+
+                            </div>
+                            <!-- END -->
+                            @if(($num + 1) % 4 == 0)
+                        </div>
+                    @endif
+                @endforeach
+
+
+            </div>
+            <!-- MORE BTN -->
+            <div class="gl-more-btn-wrapper">
+                <a href="{{ route('pageList') }}" class="gl-more-btn gl-btn">더보기</a>
+            </div>
+            <!-- END -->
+
+        </div>
+
+    </section>
+
+
+    <!-- FEATURED LISTINGS -->
+    <section class="gl-feat-listing-section gl-section-wrapper">
+        <div class="container">
+
+            <!-- SECTION HEADINGS -->
+            <div class="gl-section-headings">
+                <h1>{{ $page->title }} 채팅방</h1>
+                <p>오늘의 핫이슈</p>
+            </div>
+            <!-- END -->
+
+            <!-- WRAPPER -->
+            <div class="gl-featured-listing-wrapper">
+                @foreach($page->ssuls as $num => $chatting)
+
+                    @if(($num + 1) % 6 == 0)
+                        <div class="row">
+                        @endif
+                        <!-- FEATURED ITEMS -->
+                            <div onclick="location.href='{{ route('chattings',['name'=>$chatting->name]) }}'"
+                                 class="gl-featured-items col-md-2 col-sm-2 col-xs-6 appear fadeIn"
+                                 data-wow-duration="1s"
+                                 data-wow-delay=".3s">
+                                <div class="gl-feat-items-img-wrapper">
+
+                                    <picture>
+                                        <source media="(min-width: 768px)" src="{{ $chatting->picture }}">
+                                        <img alt="{{ $chatting->name }}" src="{{ $chatting->picture }}">
+                                    </picture>
+                                </div>
+
+                                <div class="gl-feat-item-details">
+                                    <span class="gl-item-rating">
+                                      <i class="ion-android-star-outline"></i>
+                                        {{ $chatting->loginMembersCount }}
+                                    </span>
+                                    <h3>
+                                        <a href="{{ route('pages.posts',['id'=>$chatting->id]) }}">{{ $chatting->name }}</a>
+                                    </h3>
+
+                                </div>
+                            </div>
+                            <!-- END -->
+                            @if(($num + 1)%6 == 0)
+                        </div>
+                    @endif
+                @endforeach
+
+
+            </div>
+            <!-- END -->
+
+
+        </div>
+    </section>
+    <!-- FEATURED LISTINGS END -->
+
+    <!-- FEATURED LISTINGS -->
+    <section class="gl-feat-listing-section gl-section-wrapper">
+        <div class="container">
+
+            <!-- SECTION HEADINGS -->
+            <div class="gl-section-headings">
+                <h1>베스트 채팅</h1>
+                <p>오늘의 핫이슈</p>
+            </div>
+            <!-- END -->
+
+
+            <div class="gl-post-comments-wrapper">
+                <h3 class="gl-blog-sec-title">Comments</h3>
+                <!-- Reviews -->
+                @foreach($likeBests as $best)
+                    <div class="gl-comments">
+                        <!-- USER IMG -->
+                        <div class="gl-user-img">
+                            <img src="{{ $best->profile_img }}" alt="User" class="gl-lazy">
+                        </div>
+                        <!-- END -->
+
+                        <!-- TEXT -->
+                        <div class="gl-comment-text">
+                            <div class="gl-username-date">
+
+                                <a href="{{ route('chattings', ['name' => $best->ssul_name]) }}">
+                                    <h3>{{ $best->name }}</h3>
+                                    <h3>{{ $best->ssul_name }}</h3>
+                                    <span class="gl-comments-date">{{ $best->created_at }}</span>
+                                </a>
+
+                            </div>
+                            <a href="{{ route('chattings', ['name' => $best->ssul_name]) }}"><img
+                                        src="{{ $best->picture }}"></a>
+                            <p>{{ $best->content }}</p>
+                            {{--<a href="#" class="gl-reply">Reply</a>--}}
+                        </div>
+                        <!-- END -->
+                    </div>
+            @endforeach
+            <!-- END -->
+            </div>
+
+
+        </div>
+    </section>
+    <!-- FEATURED LISTINGS END -->
+
 
 
 
